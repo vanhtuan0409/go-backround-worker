57b7d2013017ce38098366f59314a7b2cdc33edb
Tuấn Vương <vanhtuan0409@gmail.com>
2018-03-14 10:40:02 +0700

    try machinery

--- consumer/main.go
+++ consumer/main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	machinery "github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/config"
 )
 
+func intensiveAdd(id, num1, num2 int) (int, error) {
+	fmt.Printf("Start working on job %d\n", id)
+	time.Sleep(5 * time.Second)
+	ret := num1 + num2
+	fmt.Printf("Finished working on job %d\n", id)
+	return ret, nil
+}
+
 func main() {
-	fmt.Println("asadasd")
+	server, err := machinery.NewServer(&config.Config{
+		Broker:        "redis://localhost:6379/0",
+		DefaultQueue:  "my_queue",
+		ResultBackend: "redis://localhost:6379/1",
+	})
+	if err != nil {
+		panic("Cannot start server")
+	}
+
+	if err := server.RegisterTask("add", intensiveAdd); err != nil {
+		panic("Cannot register task")
+	}
+
+	worker := server.NewWorker("worker_1", 5)
+	worker.Launch()
 }
--- producer/main.go
+++ producer/main.go
@@ -2,8 +2,65 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
+
+	machinery "github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/config"
+	"github.com/RichardKnop/machinery/v1/tasks"
 )
 
+func sendTask(server *machinery.Server, id int) {
+	task := tasks.Signature{
+		Name: "add",
+		Args: []tasks.Arg{
+			{
+				Type:  "int",
+				Value: id,
+			},
+			{
+				Type:  "int",
+				Value: 4,
+			},
+			{
+				Type:  "int",
+				Value: 3,
+			},
+		},
+	}
+
+	job, err := server.SendTask(&task)
+	if err != nil {
+		fmt.Printf("Error when sending task %v\n", err)
+	}
+	fmt.Printf("Job %d sent\n", id)
+
+	maxWaitTime := time.Duration(2 * time.Second)
+	result, err := job.Get(maxWaitTime)
+	if err != nil {
+		fmt.Printf("Job %d return error: %v\n", id, err)
+	}
+	fmt.Printf("Job %d return sucess: %v\n", id, tasks.HumanReadableResults(result))
+}
+
 func main() {
-	fmt.Println("asdasda")
+	server, err := machinery.NewServer(&config.Config{
+		Broker:        "redis://localhost:6379/0",
+		DefaultQueue:  "my_queue",
+		ResultBackend: "redis://localhost:6379/1",
+	})
+	if err != nil {
+		panic("Cannot start server")
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			sendTask(server, id)
+		}(i)
+	}
+
+	wg.Wait()
 }
