c49c76219d5391c0d98d474de0943dcc936c6ab9
Tuấn Vương <vanhtuan0409@gmail.com>
2018-03-14 09:42:54 +0700

    cleanup

--- producer/main.go
+++ producer/main.go
@@ -1,29 +1,9 @@
 package main
 
 import (
-	"log"
-
-	"github.com/garyburd/redigo/redis"
-	"github.com/gocraft/work"
+	"fmt"
 )
 
-var redisPool = &redis.Pool{
-	MaxActive: 5,
-	MaxIdle:   5,
-	Wait:      true,
-	Dial: func() (redis.Conn, error) {
-		return redis.Dial("tcp", ":6379")
-	},
-}
-
-var enqueuer = work.NewEnqueuer("my_app", redisPool)
-
 func main() {
-	// Enqueue a job named "send_email" with the specified parameters.
-	for i := 0; i < 10; i++ {
-		_, err := enqueuer.Enqueue("send_email", work.Q{"id": i})
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	fmt.Println("asdasda")
 }
